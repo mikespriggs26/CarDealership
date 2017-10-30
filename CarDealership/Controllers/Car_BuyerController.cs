a3bfd1321a70570c582248d502fd80bf63af634c
mikespriggs26 <mikespriggs@msn.com>
2017-10-30 19:54:29 -0400

    first commit

---
+++ CarDealership/Controllers/Car_BuyerController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarDealership.Models;
+
+namespace CarDealership.Controllers
+{
+    public class Car_BuyerController : Controller
+    {
+        private CarDealershipContext db = new CarDealershipContext();
+
+        // GET: Car_Buyer
+        public ActionResult Index()
+        {
+            var car_Buyer = db.Car_Buyer.Include(c => c.Buyer).Include(c => c.Car);
+            return View(car_Buyer.ToList());
+        }
+
+        // GET: Car_Buyer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car_Buyer car_Buyer = db.Car_Buyer.Find(id);
+            if (car_Buyer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car_Buyer);
+        }
+
+        // GET: Car_Buyer/Create
+        public ActionResult Create()
+        {
+            ViewBag.BuyerID = new SelectList(db.Buyers, "BuyerID", "FirstName");
+            ViewBag.CarID = new SelectList(db.Cars, "CarID", "VinNumber");
+            return View();
+        }
+
+        // POST: Car_Buyer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Car_BuyerID,CarID,BuyerID")] Car_Buyer car_Buyer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Car_Buyer.Add(car_Buyer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BuyerID = new SelectList(db.Buyers, "BuyerID", "FirstName", car_Buyer.BuyerID);
+            ViewBag.CarID = new SelectList(db.Cars, "CarID", "VinNumber", car_Buyer.CarID);
+            return View(car_Buyer);
+        }
+
+        // GET: Car_Buyer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car_Buyer car_Buyer = db.Car_Buyer.Find(id);
+            if (car_Buyer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BuyerID = new SelectList(db.Buyers, "BuyerID", "FirstName", car_Buyer.BuyerID);
+            ViewBag.CarID = new SelectList(db.Cars, "CarID", "VinNumber", car_Buyer.CarID);
+            return View(car_Buyer);
+        }
+
+        // POST: Car_Buyer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Car_BuyerID,CarID,BuyerID")] Car_Buyer car_Buyer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(car_Buyer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BuyerID = new SelectList(db.Buyers, "BuyerID", "FirstName", car_Buyer.BuyerID);
+            ViewBag.CarID = new SelectList(db.Cars, "CarID", "VinNumber", car_Buyer.CarID);
+            return View(car_Buyer);
+        }
+
+        // GET: Car_Buyer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car_Buyer car_Buyer = db.Car_Buyer.Find(id);
+            if (car_Buyer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car_Buyer);
+        }
+
+        // POST: Car_Buyer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Car_Buyer car_Buyer = db.Car_Buyer.Find(id);
+            db.Car_Buyer.Remove(car_Buyer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CarDealership/Models/CarDealershipContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarDealership.Models
+{
+    public class CarDealershipContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CarDealershipContext() : base("name=CarDealershipContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CarDealership.Models.Buyer> Buyers { get; set; }
+
+        public System.Data.Entity.DbSet<CarDealership.Models.Car> Cars { get; set; }
+
+        public System.Data.Entity.DbSet<CarDealership.Models.Car_Buyer> Car_Buyer { get; set; }
+    }
+}
